990ed0a0be0fadb6b66d80e2f691d490c1296cce
Abhinav-Anand-ML <abhinav.anand@maplelabs.com>
2023-12-31 04:08:48 +0530

    Movie ist & movie card components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  variants: {
+    textColor: ['group-hover'],
+  },
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
