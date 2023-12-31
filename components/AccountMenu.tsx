5ca95913f1bf8af4c4534edf82a57d07c993eec3
Abhinav-Anand-ML <abhinav.anand@maplelabs.com>
2024-01-01 04:21:29 +0530

    Small fixes

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,11 +1,13 @@
 import { signOut } from "next-auth/react";
 import React from "react";
+import useCurrentUser from '@/hooks/useCurrentUser';
 
 interface AccountMenuProps {
     visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
+    const { data: currentUser } = useCurrentUser();
     if (!visible) {
         return null;
     }
@@ -16,12 +18,12 @@ const AccountMenu: React.FC<AccountMenuProps> = ({visible}) => {
                 <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
                     <img className="w-8 rounded-md" src="/images/default-blue.png" alt="" />
                     <p className="text-white text-sm group-hover/item:underline">
-                        UserName
+                        {currentUser?.name}
                     </p>
                 </div>
                 <hr className="bg-gray-600 border-0 h-px my-4"/>
                 <div onClick={() => signOut()} className="px-3 text-center text-white text-sm hover:underline">
-                    Sign Out
+                    Sign Out of Netflix
                 </div>
             </div>
         </div>
