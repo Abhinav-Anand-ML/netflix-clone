a78117dcea80b79ff55c0ee4284fd36f7e565232
Abhinav-Anand-ML <abhinav.anand@maplelabs.com>
2023-12-31 22:46:48 +0530

    My List component and server authentication fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,12 @@
+import { NextPageContext } from "next"
+import { getSession } from "next-auth/react"
+
 import Billboard from "@/components/Billboard";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
+
+import useFavorites from "@/hooks/useFavorites";
 import useMovieList from "@/hooks/useMovieList";
-import { NextPageContext } from "next"
-import { getSession } from "next-auth/react"
 
 export async function getServerSideProps(context: NextPageContext) {
   const session = await getSession(context);
@@ -24,6 +27,7 @@ export async function getServerSideProps(context: NextPageContext) {
 
 export default function Home() {
   const { data: movies = [] } = useMovieList();
+  const { data: favorites = [] } = useFavorites();
 
   return (
     <>
@@ -31,6 +35,7 @@ export default function Home() {
     <Billboard />
     <div className="pb-40">
       <MovieList title="Trending Now" data={movies} />
+      <MovieList title="My List" data={favorites} />
     </div>
     </>
   )
